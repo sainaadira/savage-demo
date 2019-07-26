e1812cdf4a561c17c3104c1ad7f8c2bdfdcc2e8b
Sai <snaadira@gmail.com>
2019-07-26 08:19:09 -0400

    completed and working

--- public/main.js
+++ public/main.js
@@ -1,6 +1,10 @@
 var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var thumbDown = document.getElementsByClassName("fa-thumbs-down");
 var trash = document.getElementsByClassName("fa-trash");
 
+
+
+// for thumbs up
 Array.from(thumbUp).forEach(function(element) {
       element.addEventListener('click', function(){
         const name = this.parentNode.parentNode.childNodes[1].innerText
@@ -25,21 +29,52 @@ Array.from(thumbUp).forEach(function(element) {
       });
 });
 
-Array.from(trash).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        fetch('messages', {
-          method: 'delete',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg
-          })
-        }).then(function (response) {
-          window.location.reload()
+// for thumbs down
+Array.from(thumbDown).forEach(function(element) {
+  element.addEventListener('click', function() {
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    const thumbDown = parseFloat(this.parentNode.parentNode.childNodes[7].innerText)
+    console.log(thumbDown)
+    fetch('messagesDown', {
+        method: 'put',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          'name': name,
+          'msg': msg,
+          'thumbDown': thumbDown
         })
-      });
+      })
+
+      .then(response => {
+        if (response.ok) return response.json()
+      })
+      .then(data => {
+        console.log(data)
+        window.location.reload(true)
+      })
+  });
+});
+
+
+// for the trash
+Array.from(trash).forEach(function(element) {
+  element.addEventListener('click', function() {
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    fetch('messages', {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'name': name,
+        'msg': msg
+      })
+    }).then(function(response) {
+      window.location.reload()
+    })
+  });
 });
--- server.js
+++ server.js
@@ -17,16 +17,22 @@ app.listen(3000, () => {
         console.log("Connected to `" + dbName + "`!");
     });
 });
-
+ // anything with app is express
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
+// render: and pass in message + result,  messages are objects
+  // ejs spits out html, html sent out
+  // passing an object to ejs template, messages is a property, you create word in messages
+  // messages or whatever gets sent to the ejs and called by ejs
+
 app.get('/', (req, res) => {
   //console.log(db)
   db.collection('messages').find().toArray((err, result) => {
     if (err) return console.log(err)
+    console.log(result, 'these are the messages')
     res.render('index.ejs', {messages: result})
   })
 })
@@ -40,7 +46,9 @@ app.post('/messages', (req, res) => {
 })
 
 app.put('/messages', (req, res) => {
+  // console.log('messages =""', req.body.thumbUp,'"')
   db.collection('messages')
+
   .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
     $set: {
       thumbUp:req.body.thumbUp + 1
@@ -51,8 +59,27 @@ app.put('/messages', (req, res) => {
   }, (err, result) => {
     if (err) return res.send(err)
     res.send(result)
+    // console.log('thumb up ok')
   })
 })
+
+
+app.put('/messagesDown', (req, res) => {
+  console.log('messagesDown')
+  db.collection('messages')
+  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+    $set: {
+      thumbDown:req.body.thumbDown + 1
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
 
 app.delete('/messages', (req, res) => {
   db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
